9cd37b5101e696fbf8558b497fc11565b4b0ed83
Igor Galvao <igorgalvaob@gmail.com>
2022-12-02 22:56:03 -0300

    keyboard initial setup

---
+++ src/components/keyboard/Keyboard.js
@@ -0,0 +1,28 @@
+/* import Grid from '@mui/material/Unstable_Grid2';
+import { useState, useLayoutEffect, useEffect } from 'react';
+
+const Keyboard = ({backWord, prevData})=>{
+    const [firstRow,setFirstRow] = useState[{'Q':0},{'W':0},{'E':0},{'R':0},{'T':0},{'Y':0},{'U':0},{'I':0},{'O':0},{'P':0}]
+    {
+        A:
+    }
+    const [secondRow,setSecondRow] = useState[{'A':0},{'S':0},{'D':0},{'F':0},{'G':0},{'H':0},{'J':0},{'K':0},{'L':0},{'P':0},{'delete':0}]
+    
+}
+
+export default Keyboard */
+
+const a = {
+    'A':2,
+    B:3,
+    D:4,
+    C:4,
+   
+
+
+}
+
+for(const C in a){
+    console.log(a.A)
+}
+
